3b1b2af36677c80e4fe3664527017e65d8440ba5
1101100 <joshtang255@gmail.com>
2020-09-14 18:27:01 -0700

    need to add arrow assets

--- src/Alien.java
+++ src/Alien.java
@@ -12,13 +12,15 @@ final static int UP = 0;
 final static int DOWN = 1;
 final static int LEFT = 2;
 final static int RIGHT = 3;
-
+final int maxRandomNum = 4; 
+final static int arrowUnitsAboveGhost = 60;
+int arrows = numArrow.nextInt(maxRandomNum)+1;
 	public Alien(int x, int y, int width, int height, boolean reverse) {
 		super(x, y, width, height);
 		this.reverse = reverse;
-		int arrows = numArrow.nextInt(4)+1;
+		
 		for(int i = 1; i<=arrows; i++ ) {
-			numberArrows.add(numArrow.nextInt(4));
+			numberArrows.add(numArrow.nextInt(maxRandomNum));
 		}
 	}
 	
@@ -27,8 +29,7 @@ final static int RIGHT = 3;
 		//TODO NEEDS TO SPAWN FROM BOTH X=0 & X = 500
 		
 		if (reverse) {
-			x -= 2; 
-			
+			x-=2; 
 		}
 		else {
 			x+=2;
@@ -43,7 +44,22 @@ final static int RIGHT = 3;
 
 	void draw(Graphics g) {
 		g.drawImage(GamePanel.ghost, x, y, width, height, null);
-		
+		for (int i = 1; i < numberArrows.size(); i++) {
+			
+			if(arrows == 0) {
+				g.drawImage(GamePanel.up, x, y-arrowUnitsAboveGhost, width, height, null);
+			}
+			if(arrows == 1) {
+				g.drawImage(GamePanel.down, x, y-arrowUnitsAboveGhost, width, height, null);
+			}
+			if(arrows == 2) {
+				g.drawImage(GamePanel.left, x, y-arrowUnitsAboveGhost, width, height, null);
+			}
+			if(arrows == 3) {
+				g.drawImage(GamePanel.right, x, y-arrowUnitsAboveGhost, width, height, null);
+			}
+			
+		}
 	}
 
 }
